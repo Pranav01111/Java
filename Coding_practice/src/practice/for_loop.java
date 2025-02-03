4e333ffd67ad6fb5a49936027961234c140d06cb
Pranav446 <Pranavpadwal537@gmail.com>
2025-02-03 20:26:43 +0530

    nmn

---
+++ Coding_practice/src/practice/for_loop.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class for_loop {
+
+	public static void main(String[] args) {
+
+		for (int counter = 0; counter < 11; counter = counter + 1) {
+			System.out.println(counter);
+		}
+
+	}
+
+}
